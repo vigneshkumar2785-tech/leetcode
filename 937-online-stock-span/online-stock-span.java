1ba6fb3fa5bc5cc95002ac65f3a90f53c6143cb7
vigneshkumar2785-tech <vigneshkumar2785@gmail.com>
2026-03-13 10:22:37 +0530

    Time: 35 ms (14.97%) | Memory: 56.3 MB (13.42%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,62 +1,62 @@
-// class StockSpanner {
-//     class pair{
-//         int index; 
-//         int price;
-//         pair(Integer index , Integer price)
-//         {
-//             this.price = price;
-//             this.index = index;
-//         }
-//     }
-//     Stack<pair> st = new Stack<>();
-//     int index = -1;
-
-//     public StockSpanner() {
-        
-//     }
-    
-//     public int next(int price) {
-//         index ++;
-//         pair pq = new pair(price , index);
-//         while(!st.isEmpty() && price >= st.peek().price)
-//         {
-//             st.pop();
-//         }
-//         if(st.isEmpty())
-//         {
-//             st.push(pq);
-//             return index+1;
-//         }
-//         else
-//         {
-//             int prev = st.peek().index;
-//             st.push(pq);
-//             return index - prev;
-//         }
-    
-        
-//     }
-// }
-
-
-class StockSpanner
-{
-    Stack<int[]> st;
-    public StockSpanner()
-    {
-        st = new Stack<>();
-    }
-    public int next(int price)
-    {
-        int span = 1;
-        while(!st.isEmpty() && st.peek()[0]<=price)
+class StockSpanner {
+    class pair{
+        int index; 
+        int price;
+        pair(Integer price , Integer index)
         {
-            span += st.pop()[1];
+            this.price = price;
+            this.index = index;
         }
-        st.push(new int[]{price,span});
-        return span;
+    }
+    Stack<pair> st = new Stack<>();
+    int index = -1;
+
+    public StockSpanner() {
+        
+    }
+    
+    public int next(int price) {
+        index ++;
+        pair pq = new pair(price , index);
+        while(!st.isEmpty() && price >= st.peek().price)
+        {
+            st.pop();
+        }
+        if(st.isEmpty())
+        {
+            st.push(pq);
+            return index+1;
+        }
+        else
+        {
+            int prev = st.peek().index;
+            st.push(pq);
+            return index - prev;
+        }
+    
+        
     }
 }
+
+
+// class StockSpanner
+// {
+//     Stack<int[]> st;
+//     public StockSpanner()
+//     {
+//         st = new Stack<>();
+//     }
+//     public int next(int price)
+//     {
+//         int span = 1;
+//         while(!st.isEmpty() && st.peek()[0]<=price)
+//         {
+//             span += st.pop()[1];
+//         }
+//         st.push(new int[]{price,span});
+//         return span;
+//     }
+// }
 
 /**
  * Your StockSpanner object will be instantiated and called as such:
