b76c19e45caee856f687719084e3659076ad6a6f
vigneshkumar2785-tech <vigneshkumar2785@gmail.com>
2026-03-23 14:20:11 +0530

    Time: 0 ms (100.00%) | Memory: 45.2 MB (60.14%) - LeetSync

---
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isValid(TreeNode root,long min ,long max) {
+        if(root == null) return true;
+        if(root.val<=min || root.val >= max)
+        {
+            return false;
+        } 
+        return isValid(root.left,min,root.val) && isValid(root.right,root.val,max);
+        
+    }
+    public boolean isValidBST(TreeNode root)
+    {
+        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+}
