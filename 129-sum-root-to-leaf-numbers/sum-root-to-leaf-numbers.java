28cbf34d61bcc51eb05b7f0efa4c4b2e679d69bd
vigneshkumar2785-tech <vigneshkumar2785@gmail.com>
2026-03-23 15:03:01 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (62.05%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+    private int helper(TreeNode root, int sum ){
+        if(root == null)
+        {
+            return 0;
+        }
+        sum = sum * 10 + root.val;
+        if(root.left == null && root.right ==null)
+        {
+            return sum;
+        }
+        return helper(root.left,sum) + helper(root.right, sum);
+
+        
+    }
+}
