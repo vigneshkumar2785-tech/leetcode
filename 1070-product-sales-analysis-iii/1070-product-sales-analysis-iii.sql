3fdec473380c56840cc8d1243194664c67388d76
Vigneshkumar S <vigneshkumar2785@gmail.com>
2026-09-01 12:18:58 +0530

    Time: 737 ms (66.06%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+select 
+     distinct product_id, 
+     year as first_year, 
+     quantity, 
+     price 
+     from Sales
+     where (product_id, year) in
+     (
+        select 
+            product_id,
+            min(year)
+            from Sales 
+            group by product_id 
+
+     )
